1882401189e0652a697b3e3db727c7ab94d7794d
roman <roman.seko@gmail.com>
2014-08-03 16:14:35 +0200

    index

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     </head>
     <body>
     
-<div class="container">
+<div class="container"> 
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="icon-bar"></span>
